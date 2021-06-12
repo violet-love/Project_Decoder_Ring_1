f7db113bf00100111aea8150fd81b2fd66fbddf6
Violet Love <58837492+violet-love@users.noreply.github.com>
2021-06-12 11:45:02 -0700

    wrote caesar test

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,6 @@
 // Write your tests here!
+describe('caesar', () => {
+    it('should return false if the shift amount is 0', () =>{});
+    it('should return false if the shift amount is above 25', () => {});
+    it('should return false if the shift amount is less than -25', () => {});
+})
